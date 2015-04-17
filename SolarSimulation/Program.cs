36572ef8e6ddee466d1ea15d490f5e03af05e2ad
theorclord <mikkeltheorclord@hotmail.com>
2015-04-17 13:57:31 +0200

    project setup

---
+++ SolarSimulation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace SolarSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+ 
+            foreach(DisplayIndex index in Enum.GetValues(typeof(DisplayIndex)))
+            {
+                DisplayDevice device = DisplayDevice.GetDisplay(index);
+                if (device == null)
+                {
+                    continue;
+                }
+ 
+                Console.WriteLine(device.IsPrimary);
+                Console.WriteLine(device.Bounds);
+                Console.WriteLine(device.RefreshRate);
+                Console.WriteLine(device.BitsPerPixel);
+ 
+                foreach (DisplayResolution res in device.AvailableResolutions)
+                {
+                    Console.WriteLine(res);
+                }
+            }
+        }
+    }
+}
