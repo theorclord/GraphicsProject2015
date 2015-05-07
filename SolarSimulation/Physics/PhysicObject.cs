75224d4c173b03e8bf15fcea67aaa2a70766aafc
theorclord <mikkeltheorclord@hotmail.com>
2015-05-07 18:32:40 +0200

    added phys files

---
+++ SolarSimulation/Physics/PhysicObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSimulation
+{
+    class PhysicObject
+    {
+        public double[] Acceleration { get; set; }
+        public double[] Velocity { get; set; }
+        public double mass { get; set; }
+        public double radius { get; set; }
+    }
+}
